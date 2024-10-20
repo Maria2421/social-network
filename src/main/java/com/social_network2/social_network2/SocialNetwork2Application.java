fbb711c22d134bd9e263d5945583cfb668fb9f19
Maria2421 <maria_pau.becerra@uao.edu.co>
2024-10-20 12:22:10 -0700

    first commit

---
+++ src/main/java/com/social_network2/social_network2/SocialNetwork2Application.java
@@ -0,0 +1,13 @@
+package com.social_network2.social_network2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetwork2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialNetwork2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/social_network2/social_network2/SocialNetwork2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.social_network2.social_network2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetwork2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
